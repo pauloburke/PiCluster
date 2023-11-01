c8f8cafe267927304ed8e29671f377f0eff7d7d3
Paulo Burke <pauloepburke@gmail.com>
2023-11-01 15:45:06 -0600

    Fix microk8s path

--- shutdown-cluster.sh
+++ shutdown-cluster.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Stop Microk8s on workers and shutdown
-#!/bin/bash
-
-# Stop Microk8s on workers and shutdown
-parallel-ssh -h ~/.pssh_hosts "microk8s stop && sudo shutdown now"
+parallel-ssh -i -h ~/.pssh_hosts "/snap/bin/microk8s stop && sudo shutdown now"
 
 # Stop Microk8s on master and shutdown
 microk8s stop
