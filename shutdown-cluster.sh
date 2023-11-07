7cefb7ae79c3263a4ca7a8c35ea74a67a9d17e91
Paulo Burke <pauloepburke@gmail.com>
2023-11-06 18:27:32 -0700

    Remove script

--- shutdown-cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Stop Microk8s on workers and shutdown
-parallel-ssh -i -h ~/.pssh_hosts "/snap/bin/microk8s stop && sudo shutdown now"
-
-# Stop Microk8s on master and shutdown
-microk8s stop
-sudo shutdown now
